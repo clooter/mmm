31c3f784051b2d787edfbaca77558289a47fd8f2
Sanjay Goyal <a9266321@gmail.com>
2024-11-10 16:45:10 +0000

    feat: initiated the project

--- contracts/PrivateNFT.sol
+++ contracts/PrivateNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("gg","g")
+        ERC721("hgjj","jj")
         Ownable(initialOwner)
     {}
 
